9e5aad4976baa227a2d341c8a4eb81149654e1e0
mirsery <mirsery@163.com>
2022-12-14 14:57:39 +0800

    修改了一些配色

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    // width: 1000,
+    minWidth:1000,
+    minHeight: 650,
     webPreferences: {
       nodeIntegration: true
     }
