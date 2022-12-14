7fd5e71dab5225614c03340c94ab2690b410f269
mirsery <mirsery@163.com>
2022-12-14 14:42:47 +0800

    增加了基础方法完成基础框架

--- js/easyWs.js
+++ js/easyWs.js
@@ -1,20 +1,106 @@
 let ws = require("ws");
 
-let sock = new ws("ws://localhost:8081")
+let openListener = null;
 
-sock.on("open", function () {
-    console.log("open");
-})
+let messageListener = null;
 
-sock.on("error", function () {
-    console.log("error");
-})
+let closeListener = null;
+
+let errorListener = null;
 
 
-sock.on("close", function () {
-    console.log("close");
-})
+function loadListener(sock) {
+    if (sock === null) return;
+    sock.on("open", function () {
+        if (openListener === null) return;
+        openListener.onOpen(sock);
+    })
 
-sock.on("message", function (data) {
-    console.log(data);
-})
+    sock.on("error", function (err) {
+        if (errorListener === null) return;
+        errorListener.onError(err);
+    })
+
+    sock.on("close", function () {
+        if (closeListener === null) return;
+        closeListener.onClose();
+    })
+
+    sock.on("message", function (data) {
+        if (messageListener === null) return;
+        messageListener.onMessage(sock, data);
+    })
+}
+
+function clearSock(sock) {
+    if (sock != null) {
+        sock.removeListener("open");
+        sock.removeListener("error");
+        sock.removeListener("close");
+        sock.removeListener("message");
+    }
+}
+
+
+let easyClient = {
+
+    sock: null,
+
+    uri: null,
+
+    setURL: function (url) {
+        this.uri = url;
+    },
+
+    init: function (_openListener = {
+                        onOpen: function (sock) {
+                            console.log("sock connect to server success")
+                        }
+                    },
+                    _messageListener = {
+                        onMessage: function (sock, data) {
+                            console.log("[receive] " + data);
+                        }
+                    },
+                    _closeListener = {
+                        onClose: function () {
+                            console.log("disconnect the server.")
+                        }
+                    },
+                    _errorListener = {
+                        onError: function (err) {
+                            console.log(err)
+                        }
+                    }) {
+        openListener = _openListener;
+        messageListener = _messageListener;
+        closeListener = _closeListener;
+        errorListener = _errorListener;
+    },
+
+    connect: function () {
+        if (this.uri === null) return;
+
+        if (this.sock != null) {
+            clearSock(this.sock);
+            this.sock.close();
+        }
+        this.sock = new ws(this.uri);
+        loadListener(this.sock)
+    },
+
+    send: function (msg) {
+        if (this.sock != null) {
+            this.sock.send(msg);
+        }
+    },
+
+    close: function () {
+        if (this.sock != null) {
+            this.sock.close();
+        }
+    }
+
+}
+
+export default easyClient;
